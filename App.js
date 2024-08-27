fe58aed1a07a866aecd0dc092650627fa32fac87
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-08-27 12:18:18 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [age, setAge] = useState(undefined);
+  const [lowerLimit, setLowerLimit] = useState(0);
+  const [upperLimit, setUpperLimit] = useState(0);
+
+  const calculateHeartRateLimits = () => {
+    if (!age) {
+      alert("Enter your age");
+      return;
+    }
+
+    setLowerLimit((220 - age) * 0.65);
+    setUpperLimit((220 - age) * 0.85);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age</Text>
+      <TextInput value={age} onChangeText={setAge} keyboardType="numeric" />
+      <Text>Limits</Text>
+      <Text>
+        {Math.round(lowerLimit)}-{Math.round(upperLimit)}
+      </Text>
+      <Button title="Calculate" onPress={calculateHeartRateLimits} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
 });
